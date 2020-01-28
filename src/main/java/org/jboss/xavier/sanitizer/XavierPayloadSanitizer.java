c96ee5ecd9566a4c3f497af3bf1e6e41a3969d40
m-brophy <mbrophy@redhat.com>
2020-01-28 14:29:25 +0000

    MIGENG-326: fix unable to access resource file within jar

--- src/main/java/org/jboss/xavier/sanitizer/XavierPayloadSanitizer.java
+++ src/main/java/org/jboss/xavier/sanitizer/XavierPayloadSanitizer.java
@@ -12,6 +12,7 @@ import java.nio.file.Paths;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class XavierPayloadSanitizer
 {
@@ -93,6 +94,13 @@ public class XavierPayloadSanitizer
         }
     }
 
+    private static String getShippedIssuesConditions()
+    {
+        InputStream in = XavierPayloadSanitizer.class.getClassLoader().getResourceAsStream("issues_conditions.txt");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        return reader.lines().collect(Collectors.joining("\n"));
+    }
+
 
 
     private static List<String> splitConditions(String inputConditionsString, String sectionHeader)
@@ -129,7 +137,6 @@ public class XavierPayloadSanitizer
     {
         String vmsInvalidElementIds = findInvalidElements(fileToBeSanitized, vmsIssuesConditions);
         String hostsInvalidElementIds = findInvalidElements(fileToBeSanitized, hostsIssuesConditions);
-        String clustersInvalidElementIds = findInvalidElements(fileToBeSanitized, clustersIssuesConditions);
 
         String resultString = markVmsAsRetired(fileToBeSanitized,"$.ManageIQ::Providers::Vmware::InfraManager[*].vms[?(@.id in [" + vmsInvalidElementIds+ "])]");
         resultString = markVmsAsRetired(resultString,"$.ManageIQ::Providers::Vmware::InfraManager[*].vms[?(@.host.ems_ref in [" + hostsInvalidElementIds+ "])]");
